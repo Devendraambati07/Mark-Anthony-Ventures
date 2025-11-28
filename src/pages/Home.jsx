3b477ef3b50355fbfa4818f4463557f93e2bcba4
Devendraambati07 <devendrambati@markanthony.co.in>
2025-11-28 12:00:37 +0530

    Add new favicon images in PNG and ICO formats
    - Added favicon-16x16.png and favicon-32x32.png for improved browser icon display.
    - Introduced favicon.ico for broader compatibility across different platforms and browsers.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,30 @@ import banner1 from "../assets/banner1.avif";
 import banner2 from "../assets/banner2.avif";
 import banner3 from "../assets/banner3.avif";
 
-
-const images = [banner , banner1 , banner2, banner3];
+const images = [banner, banner1, banner2, banner3];
 
 const Home = () => {
-  const [index, setIndex]= useState(0);
+  const [index, setIndex] = useState(0);
 
-  useEffect(()=>{
-    const timer =setInterval(()=>{
-      setIndex((prev)=> (prev+1)% images.length);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % images.length);
     }, 4000);
 
-    return ()=>clearInterval(timer);
+    return () => clearInterval(timer);
 
-  },[]);
-
+  }, []);
 
   return (
     <main className="relative w-full h-screen 2xl:h-[800px] overflow-hidden">
-      {images.map((img, i)=>(
-       <img key={i}
-            src={img}
-            alt=""
-            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 
+      {images.map((img, i) => (
+        <img key={i}
+          src={img}
+          alt=""
+          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 
               ${i === index ? "opacity-100" : "opacity-0"}
             `}
-          />))}
+        />))}
 
       <h1 className="absolute bottom-0 left-0 w-full  text-center poppins-bold tracking-[2px] text-[#FFD584] text-6xl md:text-8xl bg-black/40 py-1.5">
         MARK ANTHONY
